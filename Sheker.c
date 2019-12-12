becd2d326c1d094c28daa61fae605aad50ad7d6b
Ocitivka <58041753+Ocitivka@users.noreply.github.com>
2019-12-12 13:12:18 +0300

    Add files via upload

---
+++ Sheker.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+void shekerSort(double *mass, int count)
+{
+	int left = 0, right = count - 1;
+	int flag = 1;
+	printf("¬ведите числа");
+	while ((left < right) && flag > 0)
+	{
+		flag = 0;
+		for (int i = left; i<right; i++)
+		{
+			if (mass[i]>mass[i + 1])
+			{
+				double t = mass[i];
+				mass[i] = mass[i + 1];
+				mass[i + 1] = t;
+				flag = 1;
+			}
+		}
+		right--;
+		for (int i = right; i>left; i--) 
+		{
+			if (mass[i - 1]>mass[i])
+			{
+				double t = mass[i];
+				mass[i] = mass[i - 1];
+				mass[i - 1] = t;
+				flag = 1;
+			}
+		}
+		left++;
+	}
+}
+int main() {
+	double m[10];
+	for (int i = 0; i<10; i++)
+	{
+		printf("m[%d]=", i);
+		scanf("%lf", &m[i]);
+	}
+	shekerSort(m, 10);
+	for (int i = 0; i<10; i++)
+	{
+		printf("%.2lf ", m[i]);
+		getchar(); getchar();
+	}
+return 0;
+}
